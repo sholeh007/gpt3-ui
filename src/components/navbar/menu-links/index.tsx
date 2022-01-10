aea3da14dac1d19706650cfc6bc8e7ca769ca1d5
sholeh007 <sholeh09@outlook.com>
2022-01-10 12:06:34 +0900

    refactor: new menu links file

---
+++ src/components/navbar/menu-links/index.tsx
@@ -0,0 +1,21 @@
+export default function Menu() {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#wgpt3">What is GPT3</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Study</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </>
+  );
+}
