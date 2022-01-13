9b0bed274d111df01c63d61fa6a505a11c7488a1
sholeh007 <sholeh09@outlook.com>
2022-01-13 10:39:41 +0900

    finish brand section

--- src/components/brand/index.tsx
+++ src/components/brand/index.tsx
@@ -1,5 +1,23 @@
 import "./brand.css";
 
 export default function Article() {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src="/img/google.png" alt="google" />
+      </div>
+      <div>
+        <img src="/img/slack.png" alt="slack" />
+      </div>
+      <div>
+        <img src="/img/atlassian.png" alt="atlassian" />
+      </div>
+      <div>
+        <img src="/img/dropbox.png" alt="dropbox" />
+      </div>
+      <div>
+        <img src="/img/shopify.png" alt="shopify" />
+      </div>
+    </div>
+  );
 }
